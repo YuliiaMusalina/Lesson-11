fe88761f6186da5440949836933ea5055b750f4a
Ulia Kamalova <mysula@mail.ru>
2022-08-11 19:46:52 +0500

    Нашли большее число

---
+++ Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine ( " Введите первое число ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ( " Введите второе число ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+if( secondNumber > firstNumber )
+{
+    Console.WriteLine(" Второе число больше первого");
+}
+else
+{
+    Console.WriteLine("Первое число больше второго");
+}
+
+
+
